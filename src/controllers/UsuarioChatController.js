5b474d49527588e416fc91226164b3c55edf6742
Helder Andrade <helderandrade377@gmail.com>
2023-11-16 07:15:34 -0300

    UsuarioChat, UsuarioMesa - Controller

---
+++ src/controllers/UsuarioChatController.js
@@ -0,0 +1,40 @@
+const UsuarioChatService = require('../services/UsuarioChatService');
+
+const adicionarUsuarioNoChat = async (req, res) => {
+  try {
+    const { chatId } = req.params;
+    const { userId } = req.usuarioAutenticado;
+
+    const result = await UsuarioChatService.adicionarUsuarioNoChat(userId, chatId);
+
+    if (result.error) {
+      throw new Error(`Erro ao adicionar usuário no chat: ${result.error}`);
+    }
+
+    res.json({ mensagem: 'Usuário adicionado ao chat com sucesso!', chat: result.data });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+const removerUsuarioDoChat = async (req, res) => {
+  try {
+    const { chatId } = req.params;
+    const { userId } = req.usuarioAutenticado;
+
+    const result = await UsuarioChatService.removerUsuarioDoChat(userId, chatId);
+
+    if (result.error) {
+      throw new Error(`Erro ao remover usuário do chat: ${result.error}`);
+    }
+
+    res.json({ mensagem: 'Usuário removido do chat com sucesso!', chat: result.data });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+module.exports = {
+  adicionarUsuarioNoChat,
+  removerUsuarioDoChat,
+};
---
+++ src/controllers/UsuarioMesaController.js
@@ -0,0 +1,40 @@
+const UsuarioMesaService = require('../services/UsuarioMesaService');
+
+const adicionarUsuarioNaMesa = async (req, res) => {
+  try {
+    const { mesaId } = req.params;
+    const { userId } = req.usuarioAutenticado;
+
+    const result = await UsuarioMesaService.adicionarUsuarioNaMesa(userId, mesaId);
+
+    if (result.error) {
+      throw new Error(`Erro ao adicionar usuário na mesa: ${result.error}`);
+    }
+
+    res.json({ mensagem: 'Usuário adicionado à mesa com sucesso!', mesa: result.data });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+const removerUsuarioDaMesa = async (req, res) => {
+  try {
+    const { mesaId } = req.params;
+    const { userId } = req.usuarioAutenticado;
+
+    const result = await UsuarioMesaService.removerUsuarioDaMesa(userId, mesaId);
+
+    if (result.error) {
+      throw new Error(`Erro ao remover usuário da mesa: ${result.error}`);
+    }
+
+    res.json({ mensagem: 'Usuário removido da mesa com sucesso!', mesa: result.data });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+module.exports = {
+  adicionarUsuarioNaMesa,
+  removerUsuarioDaMesa,
+};
