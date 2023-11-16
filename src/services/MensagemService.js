78afbf237cfe1e09c505aab3ea8d36a10aed9636
Helder Andrade <helderandrade377@gmail.com>
2023-11-16 06:55:40 -0300

    Precisa implementar a entrada e saída das mesas

--- src/services/MensagemService.js
+++ src/services/MensagemService.js
@@ -1,8 +1,15 @@
-
+const ChatModel = require('../models/ChatModel');
+const MensagemModel = require('../models/MensagemModel');
 
 class MensagemService {
 
-    
+    static async listarMensagens(){
+        return await MensagemModel.listarMensagens();
+    }
+
+    static async lerMensagem(id){
+        return await MensagemModel.lerMensagem(id);
+    }
 
 }
 
