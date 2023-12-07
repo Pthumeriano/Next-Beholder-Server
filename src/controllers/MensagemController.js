55b1107f57b4ed5aa56a7133c74ec9573b07e57d
Helder Andrade <helderandrade377@gmail.com>
2023-12-07 06:02:23 -0300

    Retorno de erros melhorado

--- src/controllers/MensagemController.js
+++ src/controllers/MensagemController.js
@@ -1,83 +1,92 @@
-const MensagemService = require('../services/MensagemService');
+const MensagemService = require("../services/MensagemService");
 
 const listarMensagens = async (req, res) => {
-    try {
-        const {data, error} = await MensagemService.listarMensagens();
+  try {
+    const { data, error } = await MensagemService.listarMensagens();
 
-        if(error){
-            throw new Error(`Erro ao listar mensagens: ${error.message}`);
-        }
-        res.json(data)
-    } catch (error) {
-        res.status(500).json({ error: error.message });
+    if (error) {
+      throw new Error(`Erro ao listar mensagens: ${error}`);
     }
-}
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
 
 const listarMensagensChat = async (req, res) => {
-    try {
+  try {
+    const { id: chatId } = req.params;
+    const { data, error } = await MensagemService.listarMensagensChat(
+      req.usuarioAutenticado.userId,
+      chatId
+    );
 
-        const { id: chatId } = req.params;
-        const {data, error} = await MensagemService.listarMensagensChat(req.usuarioAutenticado.userId, chatId);
-
-        if(error){
-            throw new Error(`Erro ao listar mensagens: ${error.message}`);
-        }
-        res.json(data)
-    } catch (error) {
-        res.status(500).json({ error: error.message });
+    if (error) {
+      throw new Error(`Erro ao listar mensagens: ${error}`);
     }
-}
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
 
 const enviarMensagemChat = async (req, res) => {
-    try {
+  try {
+    const { id: chatId } = req.params;
+    const { data, error } = await MensagemService.enviarMensagem(
+      req.usuarioAutenticado.userId,
+      chatId,
+      req.body.mensagem
+    );
 
-        const { id: chatId } = req.params;
-        const {data, error} = await MensagemService.enviarMensagem(req.usuarioAutenticado.userId, chatId, req.body.mensagem);
-
-        if(error){
-            throw new Error(`Erro ao listar mensagens: ${error.message}`);
-        }
-        res.json(data)
-    } catch (error) {
-        res.status(500).json({ error: error.message });
+    if (error) {
+      throw new Error(`Erro ao enviar mensagem: ${error}`);
     }
-}
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
 
 const editarMensagemChat = async (req, res) => {
-    try {
+  try {
+    const { id: mensagemId } = req.params;
+    const { data, error } = await MensagemService.editarMensagem(
+      req.usuarioAutenticado.userId,
+      mensagemId,
+      req.body.texto
+    );
 
-        const { id: mensagemId } = req.params;
-        const {data, error} = await MensagemService.editarMensagem(req.usuarioAutenticado.userId, mensagemId, req.body.texto)
-
-        if(error){
-            throw new Error(`Erro ao editar mensagem: ${error.message}`);
-        }
-        res.json(data)
-    } catch (error) {
-        res.status(500).json({ error: error.message });
+    if (error) {
+      throw new Error(`Erro ao editar mensagem: ${error.message}`);
     }
-}
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
 
 const excluirMensagemChat = async (req, res) => {
-    try {
+  try {
+    const { id: mensagemId } = req.params;
+    const { data, error } = await MensagemService.excluirMensagem(
+      req.usuarioAutenticado.userId,
+      mensagemId
+    );
 
-        const { id: mensagemId } = req.params;
-        const {data, error} = await MensagemService.excluirMensagem(req.usuarioAutenticado.userId, mensagemId)
-
-        if(error){
-            throw new Error(`Erro ao excluir mensagem: ${error.message}`);
-        }
-        res.json(data)
-    } catch (error) {
-        res.status(500).json({ error: error.message });
+    if (error) {
+      throw new Error(`Erro ao excluir mensagem: ${error}`);
     }
-}
-
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
 
 module.exports = {
-    listarMensagens,
-    listarMensagensChat,
-    enviarMensagemChat,
-    editarMensagemChat,
-    excluirMensagemChat
-}
+  listarMensagens,
+  listarMensagensChat,
+  enviarMensagemChat,
+  editarMensagemChat,
+  excluirMensagemChat,
+};
