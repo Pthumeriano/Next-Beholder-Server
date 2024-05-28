231da27da128037e7e6b152704685b55d2a669ce
`Helder` <`helderandrade377@gmail.com`>
2024-05-27 21:44:20 -0300

    Middleware validações do usuário

--- src/middlewares/ValidacaoMiddleware.js
+++ src/middlewares/ValidacaoMiddleware.js
@@ -10,7 +10,12 @@ const validarNovoUsuario = [
       return true;
     }),
   body('email').isEmail().withMessage('O email deve ser válido'),
-  body('senha').isLength({ min: 6 }).withMessage('A senha deve ter pelo menos 6 caracteres'),
+  body('senha')
+    .isLength({ min: 8 }).withMessage('A senha deve ter pelo menos 8 caracteres')
+    .matches(/[A-Z]/).withMessage('A senha deve conter pelo menos uma letra maiúscula')
+    .matches(/[a-z]/).withMessage('A senha deve conter pelo menos uma letra minúscula')
+    .matches(/\d/).withMessage('A senha deve conter pelo menos um número')
+    .matches(/[!@#$%^&*]/).withMessage('A senha deve conter pelo menos um caractere especial (!@#$%^&*)'),
 
   (req, res, next) => {
     const errors = validationResult(req);
@@ -20,7 +25,6 @@ const validarNovoUsuario = [
     next();
   },
 ];
-
 
 const validarAlteracaoUsuario = [
   body('nome')
@@ -42,7 +46,6 @@ const validarAlteracaoUsuario = [
     .isLength({ max: 100 }).withMessage('A descrição não pode ter mais de 100 caracteres'),
 
   (req, res, next) => {
-    
     const camposIndesejados = ['email', 'senha'];
     const corpoRequisicao = Object.keys(req.body);
 
@@ -63,7 +66,11 @@ const validarAlteracaoUsuario = [
 const validarAtualizacaoSenha = [
   body('senhaAntiga').notEmpty().withMessage('Senha antiga é obrigatória'),
   body('novaSenha')
-    .isLength({ min: 6 }).withMessage('A nova senha deve ter pelo menos 6 caracteres')
+    .isLength({ min: 8 }).withMessage('A nova senha deve ter pelo menos 8 caracteres')
+    .matches(/[A-Z]/).withMessage('A nova senha deve conter pelo menos uma letra maiúscula')
+    .matches(/[a-z]/).withMessage('A nova senha deve conter pelo menos uma letra minúscula')
+    .matches(/\d/).withMessage('A nova senha deve conter pelo menos um número')
+    .matches(/[!@#$%^&*]/).withMessage('A nova senha deve conter pelo menos um caractere especial (!@#$%^&*)')
     .custom((value, { req }) => {
       if (value === req.body.senhaAntiga) {
         throw new Error('A nova senha deve ser diferente da senha antiga');
@@ -80,82 +87,82 @@ const validarAtualizacaoSenha = [
   },
 ];
 
-  const validarLogin = [
-    body('email').isEmail().withMessage('O email deve ser válido'),
-    body('senha').isLength({ min: 6 }).withMessage('A senha deve ter pelo menos 6 caracteres'),
+const validarLogin = [
+  body('email').isEmail().withMessage('O email deve ser válido'),
+  body('senha').isLength({ min: 6 }).withMessage('A senha deve ter pelo menos 6 caracteres'),
   
-    (req, res, next) => {
-      const errors = validationResult(req);
-      if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
-      }
-      next();
-    },
-  ];
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    next();
+  },
+];
 
-  const validarCriacaoMesa = [
-    body('titulo')
-      .notEmpty().withMessage('O título é obrigatório')
-      .isLength({ max: 20 }).withMessage('O título não pode ter mais de 30 caracteres'),
+const validarCriacaoMesa = [
+  body('titulo')
+    .notEmpty().withMessage('O título é obrigatório')
+    .isLength({ max: 20 }).withMessage('O título não pode ter mais de 30 caracteres'),
   
-    body('subtitulo')
-      .notEmpty().withMessage('O subtitulo é obrigatório')
-      .isLength({ max: 30 }).withMessage('O subtitulo não pode ter mais de 50 caracteres'),
+  body('subtitulo')
+    .notEmpty().withMessage('O subtitulo é obrigatório')
+    .isLength({ max: 30 }).withMessage('O subtitulo não pode ter mais de 50 caracteres'),
   
-    body('sistema')
-      .notEmpty().withMessage('O sistema é obrigatório')
-      .isLength({ max: 30 }).withMessage('O sistema não pode ter mais de 30 caracteres'),
+  body('sistema')
+    .notEmpty().withMessage('O sistema é obrigatório')
+    .isLength({ max: 30 }).withMessage('O sistema não pode ter mais de 30 caracteres'),
   
-    body('descricao')
-      .notEmpty().withMessage('A descrição é obrigatória')
-      .isLength({ max: 100 }).withMessage('A descrição não pode ter mais de 100 caracteres'),
+  body('descricao')
+    .notEmpty().withMessage('A descrição é obrigatória')
+    .isLength({ max: 100 }).withMessage('A descrição não pode ter mais de 100 caracteres'),
   
-    body('dia')
-      .notEmpty().withMessage('O dia é obrigatório'),
+  body('dia')
+    .notEmpty().withMessage('O dia é obrigatório'),
   
-    body('horario')
-      .notEmpty().withMessage('O horário é obrigatório')
-      .isLength({ max: 10 }).withMessage('O horário não pode ter mais de 10 caracteres'),
+  body('horario')
+    .notEmpty().withMessage('O horário é obrigatório')
+    .isLength({ max: 10 }).withMessage('O horário não pode ter mais de 10 caracteres'),
   
-    body('periodo')
-      .notEmpty().withMessage('O período é obrigatório')
-      .isLength({ max: 10 }).withMessage('O período não pode ter mais de 10 caracteres'),
+  body('periodo')
+    .notEmpty().withMessage('O período é obrigatório')
+    .isLength({ max: 10 }).withMessage('O período não pode ter mais de 10 caracteres'),
   
-    body('preco')
-      .notEmpty().withMessage('O preço é obrigatório')
-      .isNumeric().withMessage('O preço deve ser um número')
-      .custom(value => value <= 10).withMessage('O preço não pode ser maior que 10'),
+  body('preco')
+    .notEmpty().withMessage('O preço é obrigatório')
+    .isNumeric().withMessage('O preço deve ser um número')
+    .custom(value => value <= 10).withMessage('O preço não pode ser maior que 10'),
   
-    body('vagas')
-      .notEmpty().withMessage('O número de vagas é obrigatório')
-      .isInt().withMessage('O número de vagas deve ser um número inteiro')
-      .custom(value => value <= 15).withMessage('O número de vagas não pode ser maior que 15'),
+  body('vagas')
+    .notEmpty().withMessage('O número de vagas é obrigatório')
+    .isInt().withMessage('O número de vagas deve ser um número inteiro')
+    .custom(value => value <= 15).withMessage('O número de vagas não pode ser maior que 15'),
   
-    (req, res, next) => {
-      const errors = validationResult(req);
+  (req, res, next) => {
+    const errors = validationResult(req);
   
-      if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
-      }
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
   
-      next();
-    },
-  ];
+    next();
+  },
+];
 
-  const validarEmail = [
-    body('email')
-      .notEmpty().withMessage('O e-mail é obrigatório')
-      .isEmail().withMessage('E-mail inválido'),
+const validarEmail = [
+  body('email')
+    .notEmpty().withMessage('O e-mail é obrigatório')
+    .isEmail().withMessage('E-mail inválido'),
   
-    (req, res, next) => {
-      const errors = validationResult(req);
+  (req, res, next) => {
+    const errors = validationResult(req);
   
-      if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
-      }
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
   
-      next();
-    },
-  ];
+    next();
+  },
+];
 
-module.exports = { validarNovoUsuario, validarAtualizacaoSenha, validarAlteracaoUsuario, validarLogin, validarCriacaoMesa, validarEmail  };
+module.exports = { validarNovoUsuario, validarAtualizacaoSenha, validarAlteracaoUsuario, validarLogin, validarCriacaoMesa, validarEmail };
